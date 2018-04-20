ae2eb482abf851ad91a5ecd2741b70f603656e21
rickoff <38435435+rickoff@users.noreply.github.com>
2018-04-20 12:44:02 +0200

    Update botdiscordtes3mp.py

--- botdiscordtes3mp.py
+++ botdiscordtes3mp.py
@@ -7,109 +7,122 @@ import time
 client = discord.Client()
 list_all_user = []
 list_all_channel = []
-list_channelID_admin = ["replace to id channel discord to conserve","replace to id channel discord to conserve"]
-playerlocationjson = 'destination playerlocation.json'
+list_channelID_admin = ["replace to id channel","replace to id channel"]
+playerlocationjson = 'C:\\Morrowind Online Serveur VF 0.6.1\\The Elder Scrolls III Morrowind GOTY\\tes3mp.Win64.release.0.7.0\\mp-stuff\\data\\playerLocations.json'
 
 @client.event
 async def on_ready():
-	print('Connect')
+	print('Connecté')
 	print(client.user.name)
 	print(client.user.id)
 	print('------')
+	
 	await client.wait_until_ready()
 	while not client.is_closed:
+		try:
 		
-		server = client.get_server("replace to id server")
+			server = client.get_server("replace to id server")
+		
+			with open(playerlocationjson) as json_data:
+				d = json.load(json_data)
 
-		with open(playerlocationjson) as json_data:
-			d = json.load(json_data)
-
-		list_all_player = d['players']
-		list_cellAndPlayer = []
-		for elt in list_all_player:
-			name_ingame = elt['name']
-			cell_ingame = elt['cell']			
-			new_list = {
-				'name': elt['name'],
-				'cell': elt['cell']
-			}
-			list_cellAndPlayer.append(new_list) 
-
-		list_all_user = []
-		for member in server.members:
-			dico_info = {
-				'display_name': member.display_name,
-				'id_user': member.id,
-				'nick_name': member.name
-			}
-			list_all_user.append(dico_info)
-
-		list_all_channel = []
-		for channel in server.channels:			
-			dico_info = {
-				'name_channel_discord': channel.name,
-				'id_channel_discord': channel.id,
-				'type_channel': channel.type
-			}
-			list_all_channel.append(dico_info)
-
-		for nameCell in list_cellAndPlayer:
-			find_channel = discord.utils.find(lambda m: m.name == nameCell['cell'], server.channels)
-
-			if find_channel == None:
-				everyone = discord.PermissionOverwrite(read_messages=False)
-				await client.create_channel(server, nameCell['cell'], (server.default_role, everyone), type=discord.ChannelType.voice)
-				print('nouveau channel crée')	
-
-			elif find_channel != None:
-				print('channel exist')
-
-		for i in list_cellAndPlayer:
-			name_ingame = i['name']
-			cell_ingame = i['cell']			
+			list_all_player = d['players']
 			
-			for elt in list_all_user:
-				name_discord = elt['display_name']
-				id_user_discord = elt['id_user']
+			list_cellAndPlayer = []
+			for elt in list_all_player:
+				new_list = {
+					'name': elt['name'],
+					'cell': elt['cell']
+				}
+				list_cellAndPlayer.append(new_list)
+				#print(new_list)
 
-				if name_ingame == name_discord:
+			list_all_user = []
+			for member in server.members:
+				dico_info = {
+					'display_name': member.display_name,
+					'id_user': member.id,
+					'nick_name': member.name
+				}
+				list_all_user.append(dico_info)
 
-					for info in list_all_channel:
-						name_channel_discord = info['name_channel_discord']
-						id_channel_discord = info['id_channel_discord']
+			list_all_channel = []
+			for channel in server.channels:			
+				dico_info = {
+					'name_channel_discord': channel.name,
+					'id_channel_discord': channel.id,
+					'type_channel': channel.type
+				}
+				list_all_channel.append(dico_info)
+					
+			for nameCell in list_cellAndPlayer:
+				find_channel = discord.utils.find(lambda m: m.name == nameCell['cell'], server.channels)
+				try :
+				
+					if find_channel == None:
+							everyone = discord.PermissionOverwrite(read_messages=False)
+							await client.create_channel(server, nameCell['cell'], (server.default_role, everyone), type=discord.ChannelType.voice)
+							print('nouveau channel crée')
+							await asyncio.sleep(0.5)	
 						
-						if name_channel_discord == cell_ingame:
-							channel = client.get_channel(id_channel_discord)
-							
-							if channel != None:
-								print('channel %s exist in server !'%(channel))
-								
-								if channel.type == discord.ChannelType.voice:
-									user = server.get_member(id_user_discord)
-									
-									if user != None:
-										await client.move_member(user, channel)
-										print('User %s move ! big up'%(name_discord))
-										await asyncio.sleep(1)
+					elif find_channel != None:
+						#print('le channel existe déja')
+						pass
+				except:
+					print('erreur de creation')
+					await asyncio.sleep(0.5)
+					
+			for i in list_cellAndPlayer:
+				name_ingame = i['name']
+				cell_ingame = i['cell']			
+				
+				for elt in list_all_user:
+					name_discord = elt['display_name']
+					id_user_discord = elt['id_user']
+					try:
+					
+						if name_ingame == name_discord:
 
-		for info in list_all_channel:
-			name_channel_discord = info['name_channel_discord']
-			id_channel_discord = info['id_channel_discord']				
-			channel = client.get_channel(id_channel_discord)
-			
-			if not channel.id in list_channelID_admin and channel.type == discord.ChannelType.voice:
-			
-				if not channel.voice_members:
-				
-					for i in list_cellAndPlayer:
-						name_ingame = i['name']
-						cell_ingame = i['cell']					
-				
-						if cell_ingame != name_channel_discord:						
-							await client.delete_channel(channel)
-							print('le channel %s a était delete.'%(channel.name))
-							await asyncio.sleep(1)							
-	
+							for info in list_all_channel:
+								name_channel_discord = info['name_channel_discord']
+								id_channel_discord = info['id_channel_discord']
+								
+								if name_channel_discord == cell_ingame:
+									channel = client.get_channel(id_channel_discord)
+									
+									if channel != None:
+										
+										if channel.type == discord.ChannelType.voice:
+											user = server.get_member(id_user_discord)
+											
+											if user != None:
+												await client.move_member(user, channel)
+												print('User %s déplacé avec succès ! big up'%(name_discord))
+												await asyncio.sleep(0.5)
+					except:
+						print('erreur de deplacement')
+						await asyncio.sleep(0.5)
+									
+			for info in list_all_channel:
+				name_channel_discord = info['name_channel_discord']
+				id_channel_discord = info['id_channel_discord']				
+				channel = client.get_channel(id_channel_discord)
+				try:
+					
+					if not channel.id in list_channelID_admin and channel.type == discord.ChannelType.voice:
+					
+						if not channel.voice_members:					
+						
+							if cell_ingame != name_channel_discord:						
+								await client.delete_channel(channel)
+								print('le channel %s a était delete.'%(channel.name))
+								await asyncio.sleep(0.5)
+				except:
+					print('erreur de suppression')
+					await asyncio.sleep(0.5)
+		except:
+			print('erreur1')
+			await asyncio.sleep(0.5)
 pass
-																	
+																															
 client.run('replace to token bot')
