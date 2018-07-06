055b5731b56ca22b35539974d1b7ce6ffd3d6c22
rickoff <38435435+rickoff@users.noreply.github.com>
2018-07-07 00:43:52 +0200

    Create LevelMaxCap.lua

---
+++ LevelMaxCap.lua
@@ -0,0 +1,14 @@
+--go to player>base.lua
+--find ``function BasePlayer:SaveSkills()``
+--add under ``local maxSkillValue = config.maxSkillValue`` 
+
+		local maxlevel = 50
+
+		if self.data.stats.level >= maxlevel then
+			tes3mp.SetLevel(self.pid, maxlevel)
+			tes3mp.SendSkills(self.pid)
+			if self.data.stats.levelProgress >= 10 then
+				tes3mp.SetLevelProgress(self.pid, 0)
+				tes3mp.SendSkills(self.pid)
+			end
+		end
