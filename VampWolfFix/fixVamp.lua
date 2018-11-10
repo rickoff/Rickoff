a1707d0804526ce544c010f062128376016fa4fb
rickoff <38435435+rickoff@users.noreply.github.com>
2018-11-10 11:11:13 +0100

    Update fixVamp.lua

--- VampWolfFix/fixVamp.lua
+++ VampWolfFix/fixVamp.lua
@@ -2,53 +2,43 @@
 
 --INSTALLATION
 --add fixVamp.lua file in mp-stuff/script
---open server.lua and find function OnPlayerCellChange(pid) and a add in :
+--open serverCore.lua and find function OnPlayerCellChange(pid) and a add in :
 --fixVamp.tcheckVamp(pid)	
 --fixVamp.tcheckWolf(pid)
- 
+
 local fixVamp = {}	
 	
 fixVamp.tcheckVamp = function(pid)	
-    local vampireClan
+
     local consoleTcheckvamp
+	
 	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-		for index, item in pairs(Players[pid].data.spellbook) do
-			if tableHelper.containsKeyValue(Players[pid].data.spellbook, "spellId", "vampire berne specials", true) then
-				vampireClan = "berne"
-			elseif tableHelper.containsKeyValue(Players[pid].data.spellbook, "spellId", "vampire aundae specials", true) then
-				vampireClan = "aundae"
-			elseif tableHelper.containsKeyValue(Players[pid].data.spellbook, "spellId", "vampire quarra specials", true) then
-				vampireClan = "quarra"
-			else
-				vampireClan = "none"
+		for slot, k in pairs(Players[pid].data.spellbook) do
+			if Players[pid].data.spellbook[slot] == "vampire berne specials" then
+				consoleTcheckvamp = "set PCVampire to 1"
+				logicHandler.RunConsoleCommandOnPlayer(pid, consoleTcheckvamp)	
+			elseif Players[pid].data.spellbook[slot] == "vampire aundae specials" then
+				consoleTcheckvamp = "set PCVampire to 1"
+				logicHandler.RunConsoleCommandOnPlayer(pid, consoleTcheckvamp)	
+			elseif Players[pid].data.spellbook[slot] == "vampire quarra specials" then
+				consoleTcheckvamp = "set PCVampire to 1"
+				logicHandler.RunConsoleCommandOnPlayer(pid, consoleTcheckvamp)					
 			end
-		end
-		if vampireClan ~= "none" then
-			consoleTcheckvamp = "set PCVampire to 1"
-		else
-			consoleTcheckvamp = "set PCVampire to 0"
-		end
-		myMod.RunConsoleCommandOnPlayer(pid, consoleTcheckvamp)
+		end	
 	end
 end
 
 fixVamp.tcheckWolf = function(pid)	
-	local playerWolf
+
 	local consoleTcheckwolf
+	
 	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-		for index, item in pairs(Players[pid].data.spellbook) do
-			if tableHelper.containsKeyValue(Players[pid].data.spellbook, "spellId", "werewolf blood", true) then
-				playerWolf = "true"
-			else
-				playerWolf = "false"
+		for slot, k in pairs(Players[pid].data.spellbook) do
+			if Players[pid].data.spellbook[slot] == "werewolf blood" then
+				consoleTcheckwolf = "set PCWerewolf to 1"
+				logicHandler.RunConsoleCommandOnPlayer(pid, consoleTcheckwolf)	
 			end
-		end
-		if playerWolf ~= "false" then
-			consoleTcheckwolf = "set PCWerewolf to 1"
-		else
-			consoleTcheckwolf = "set PCWerewolf to 0"
-		end
-		myMod.RunConsoleCommandOnPlayer(pid, consoleTcheckwolf)
+		end		
 	end
 end
 
