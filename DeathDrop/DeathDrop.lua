ff826ec2b0b221bee6d472a3c7a525088a89c3fa
rickoff <38435435+rickoff@users.noreply.github.com>
2019-01-26 16:50:11 +0100

    Update DeathDrop.lua

--- DeathDrop/DeathDrop.lua
+++ DeathDrop/DeathDrop.lua
@@ -1,10 +1,23 @@
 -- This is the modified version of DeathDrop made for Rickoff so that only gold is dropped on death.
--- tes3mp 0.7.0
+
 -- DeathDrop.lua -*-lua-*-
+-- "THE BEER-WARE LICENCE" (Revision 42):
+-- <mail@michael-fitzmayer.de> wrote this file.  As long as you retain
+-- this notice you can do whatever you want with this stuff. If we meet
+-- some day, and you think this stuff is worth it, you can buy me a beer
+-- in return.  Michael Fitzmayer
+
 
 Methods = {}
 
 inventoryHelper = require("inventoryHelper")
+
+-- Add [ DeathDrop = require("DeathDrop") ] to the top of myMod.lua
+
+-- Find "Players[pid]:ProcessDeath()" inside myMod.lua and add:
+-- [ DeathDrop.Drop(pid) ]
+-- directly above it.
+
 
 Methods.Drop = function(pid)
 	local player = Players[pid]
@@ -16,20 +29,25 @@ Methods.Drop = function(pid)
 			
 		local mpNum = WorldInstance:GetCurrentMpNum() + 1
 		local cell = tes3mp.GetCell(pid)
-		local location = {
-			posX = tes3mp.GetPosX(pid), posY = tes3mp.GetPosY(pid), posZ = tes3mp.GetPosZ(pid),
-		}
+        local location = {
+            posX = tes3mp.GetPosX(pid), posY = tes3mp.GetPosY(pid), posZ = tes3mp.GetPosZ(pid),
+            rotX = tes3mp.GetRotX(pid), rotY = 0, rotZ = tes3mp.GetRotZ(pid)
+        }
 		local refId = item.refId
 		local count = (item.count - 100)
 		local refIndex =  0 .. "-" .. mpNum
+		local itemref = {refId = "gold_001", count = (count), charge = -1}		
+		player.data.inventory[goldLoc].count = player.data.inventory[goldLoc].count - count	
+		Players[pid]:Save()
+		Players[pid]:LoadItemChanges({itemref}, enumerations.inventory.REMOVE)	
 		
 		WorldInstance:SetCurrentMpNum(mpNum)
 		tes3mp.SetCurrentMpNum(mpNum)
 
-		LoadedCells[cell]:InitializeObjectData(refIndex, refId)
-		LoadedCells[cell].data.objectData[refIndex].location = location
+		LoadedCells[cell]:InitializeObjectData(refIndex, refId)		
+		LoadedCells[cell].data.objectData[refIndex].location = location		
+		LoadedCells[cell].data.objectData[refIndex].goldValue = count		
 		table.insert(LoadedCells[cell].data.packets.place, refIndex)
-		LoadedCells[cell]:Save()
 
 		for onlinePid, player in pairs(Players) do
 			if player:IsLoggedIn() then
@@ -39,20 +57,15 @@ Methods.Drop = function(pid)
 				tes3mp.SetObjectCount(count)
 				tes3mp.SetObjectRefNumIndex(0)
 				tes3mp.SetObjectMpNum(mpNum)
-
 				tes3mp.SetObjectPosition(location.posX, location.posY, location.posZ)
+				tes3mp.SetObjectRotation(location.rotX, location.rotY, location.rotZ)
 				tes3mp.AddWorldObject()
 				tes3mp.SendObjectPlace()
 			end
 		end
-		
-		player.data.inventory[goldLoc] = {refId = "gold_001", count = 100, charge = -1}
-		
-		local itemref = {refId = "gold_001", count = 100, charge = -1}			
-		Players[pid]:Save()
-		Players[pid]:LoadItemChanges({itemref}, enumerations.inventory.ADD)			
-
+		LoadedCells[cell]:Save()		
 	end
 end
+
 
 return Methods
