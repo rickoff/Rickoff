fab3ffc8341198046dab41ccb2a02d8d77e584f9
rickoff <38435435+rickoff@users.noreply.github.com>
2019-10-31 12:47:17 +0100

    Dynamic move place object

--- Housing/decorateHelp.lua
+++ Housing/decorateHelp.lua
@@ -19,6 +19,7 @@
 
 ]]
 
+tableHelper = require("tableHelper")
 ------
 local config = {}
 config.MainId = 31360
@@ -40,16 +41,37 @@ trad.north = "Nord"
 trad.sud = "Sud"
 trad.bigger = "Agrandir"
 trad.lower = "Réduire"
+trad.drop = "Attraper"
+trad.noselect = "Aucun objet sélectionné."
+trad.nooption = "Objet impossible à modifier."
+trad.placeobjet = "L'objet vient d'être placé."
+trad.info = "Pour poser l'objet passez en mode discretion."
 trad.opt1 = "Choisir une option. Votre article actuel: "
-trad.opt2 = "Ajuster le Nord;Ajuster l'est;Ajuster la hauteur;Tourner X;Tourner Y;Tourner Z;Monter;Descendre;Est;Ouest;Nord;Sud;Agrandir;Réduire;Fermer"
+trad.opt2 = "Ajuster le Nord;Ajuster l'est;Ajuster la hauteur;Tourner X;Tourner Y;Tourner Z;Monter;Descendre;Est;Ouest;Nord;Sud;Agrandir;Réduire;Attraper;Fermer"
 ------
 
-Methods = {}
-
-tableHelper = require("tableHelper")
+local Methods = {}
 
 local playerSelectedObject = {}
 local playerCurrentMode = {}
+local playersTab = { player = {} }
+local TimerDrop = tes3mp.CreateTimer("StartDrop", time.seconds(0.01))
+
+function StartDrop()	
+	for pid, value in pairs(playersTab.player) do
+		if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
+			local namep = playersTab.player[pid].name
+			for pid1, value in pairs(Players) do	
+				if Players[pid1] ~= nil and Players[pid1]:IsLoggedIn() then			
+					local name = Players[pid].name
+					if name == namep then
+						Methods.moveObject(pid1)
+					end
+				end
+			end
+		end
+	end
+end
 
 local function getObject(refIndex, cell)
 	if refIndex == nil then
@@ -75,6 +97,8 @@ local function resendPlaceToAll(refIndex, cell)
 	local charge = object.charge or -1
 	local posX, posY, posZ = object.location.posX, object.location.posY, object.location.posZ
 	local rotX, rotY, rotZ = object.location.rotX, object.location.rotY, object.location.rotZ
+	local scale = object.scale
+
 	local refIndex = refIndex
 	
 	local inventory = object.inventory or nil
@@ -99,6 +123,7 @@ local function resendPlaceToAll(refIndex, cell)
 			tes3mp.SetObjectCharge(charge)
 			tes3mp.SetObjectPosition(posX, posY, posZ)
 			tes3mp.SetObjectRotation(rotX, rotY, rotZ)
+			tes3mp.SetObjectScale(scale)
 			tes3mp.SetObjectRefNumIndex(0)
 			tes3mp.SetObjectMpNum(splitIndex[2])
 			if inventory then
@@ -113,6 +138,7 @@ local function resendPlaceToAll(refIndex, cell)
 			
 			tes3mp.AddWorldObject()
 			tes3mp.SendObjectPlace()
+			tes3mp.SendObjectScale()
 			if inventory then
 				tes3mp.SendContainer()
 			end
@@ -134,51 +160,67 @@ local function onEnterPrompt(pid, data)
 	local pname = tes3mp.GetName(pid)
 	local mode = playerCurrentMode[pname]
 	local data = tonumber(data) or 0
-	
 	local object = getObject(playerSelectedObject[pname], cell)
 	
 	if not object then
-		--The object no longer exists, so we should bail out now
+		tes3mp.MessageBox(pid, -1, trad.noselect)	
 		return false
-	end
-	
-	if mode == trad.rotx then
-		local curDegrees = math.deg(object.location.rotX)
-		local newDegrees = (curDegrees + data) % 360
-		object.location.rotX = math.rad(newDegrees)
-	elseif mode == trad.roty then
-		local curDegrees = math.deg(object.location.rotY)
-		local newDegrees = (curDegrees + data) % 360
-		object.location.rotY = math.rad(newDegrees)
-	elseif mode == trad.rotz then
-		local curDegrees = math.deg(object.location.rotZ)
-		local newDegrees = (curDegrees + data) % 360
-		object.location.rotZ = math.rad(newDegrees)
-	elseif mode == trad.movn then
-		object.location.posY = object.location.posY + data
-	elseif mode == trad.move then
-		object.location.posX = object.location.posX + data
-	elseif mode == trad.movup then
-		object.location.posZ = object.location.posZ + data
-	elseif mode == trad.up then
-		object.location.posZ = object.location.posZ + 10
-	elseif mode == trad.down then
-		object.location.posZ = object.location.posZ - 10
-	elseif mode == trad.east then
-		object.location.posX = object.location.posX + 10
-	elseif mode == trad.west then
-		object.location.posX = object.location.posX - 10
-	elseif mode == trad.north then
-		object.location.posY = object.location.posY + 10
-	elseif mode == trad.sud then
-		object.location.posY = object.location.posY - 10
-	elseif mode == trad.bigger then
-		logicHandler.RunConsoleCommandOnObject("setscale (getscale + 0.1)", cell, playerSelectedObject[pname])
-	elseif mode == trad.lower then
-		logicHandler.RunConsoleCommandOnObject("setscale (getscale - 0.1)", cell, playerSelectedObject[pname])		
-	elseif mode == "return" then
-		object.location.posY = object.location.posY		
-		return
+	else
+		local scaling = object.scale	
+		if mode == trad.rotx then
+			local curDegrees = math.deg(object.location.rotX)
+			local newDegrees = (curDegrees + data) % 360
+			object.location.rotX = math.rad(newDegrees)
+		elseif mode == trad.roty then
+			local curDegrees = math.deg(object.location.rotY)
+			local newDegrees = (curDegrees + data) % 360
+			object.location.rotY = math.rad(newDegrees)
+		elseif mode == trad.rotz then
+			local curDegrees = math.deg(object.location.rotZ)
+			local newDegrees = (curDegrees + data) % 360
+			object.location.rotZ = math.rad(newDegrees)
+		elseif mode == trad.movn then
+			object.location.posY = object.location.posY + data
+		elseif mode == trad.move then
+			object.location.posX = object.location.posX + data
+		elseif mode == trad.movup then
+			object.location.posZ = object.location.posZ + data
+		elseif mode == trad.up then
+			object.location.posZ = object.location.posZ + 10
+		elseif mode == trad.down then
+			object.location.posZ = object.location.posZ - 10
+		elseif mode == trad.east then
+			object.location.posX = object.location.posX + 10
+		elseif mode == trad.west then
+			object.location.posX = object.location.posX - 10
+		elseif mode == trad.north then
+			object.location.posY = object.location.posY + 10
+		elseif mode == trad.sud then
+			object.location.posY = object.location.posY - 10
+		elseif mode == trad.bigger then
+			if scaling ~= nil then
+				if scaling < 2 then
+					object.scale = object.scale + 0.1
+				else
+					object.scale = object.scale
+				end
+			else
+				tes3mp.MessageBox(pid, -1, trad.nooption)		
+			end
+		elseif mode == trad.lower then
+			if scaling ~= nil then
+				if scaling > 0.1 then
+					object.scale = object.scale - 0.1
+				else
+					object.scale = object.scale
+				end
+			else
+				tes3mp.MessageBox(pid, -1, trad.nooption)		
+			end		
+		elseif mode == "return" then
+			object.location.posY = object.location.posY		
+			return
+		end
 	end
 	
 	resendPlaceToAll(playerSelectedObject[pname], cell)
@@ -200,6 +242,10 @@ end
 
 local function setSelectedObject(pid, refIndex)
 	playerSelectedObject[tes3mp.GetName(pid)] = refIndex
+end
+
+Methods.StartDropTimer = function()
+	tes3mp.StartTimer(TimerDrop)
 end
 
 Methods.SetSelectedObject = function(pid, refIndex)
@@ -276,8 +322,13 @@ Methods.OnGUIAction = function(pid, idGui, data)
 		elseif tonumber(data) == 13 then --Reduire
 			playerCurrentMode[pname] = trad.lower
 			onEnterPrompt(pid, 0)
-			return true, showMainGUI(pid)				
-		elseif tonumber(data) == 14 then --Close
+			return true, showMainGUI(pid)	
+		elseif tonumber(data) == 14 then --Attraper
+			playersTab.player[pid] = {name = Players[pid].name}
+			tes3mp.MessageBox(pid, -1, trad.info)	
+			Methods.StartDropTimer()
+			return true				
+		elseif tonumber(data) == 15 then --Close
 			--Do nothing
 			return true
 		end
@@ -288,6 +339,44 @@ Methods.OnGUIAction = function(pid, idGui, data)
 		
 		playerCurrentMode[tes3mp.GetName(pid)] = nil
 		return true, showMainGUI(pid)
+	end
+end
+
+Methods.moveObject = function(pid)
+	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
+		local cell = tes3mp.GetCell(pid)
+		local pname = tes3mp.GetName(pid)
+		local object = getObject(playerSelectedObject[pname], cell)
+		if not object then
+			tes3mp.MessageBox(pid, -1, trad.noselect)	
+			return false
+		else
+			local playerAngleZ = tes3mp.GetRotZ(pid)
+			if playerAngleZ > 3.0 then
+				playerAngleZ = 3.0
+			elseif playerAngleZ < -3.0 then
+				playerAngleZ = -3.0
+			end
+			local playerAngleX = tes3mp.GetRotX(pid)
+			if playerAngleX > 1.5 then
+				playerAngleX = 1.5
+			elseif playerAngleX < -1.5 then
+				playerAngleX = -1.5
+			end			
+			local PosX = (200 * math.sin(playerAngleZ) + tes3mp.GetPosX(pid))
+			local PosY = (200 * math.cos(playerAngleZ) + tes3mp.GetPosY(pid))
+			local PosZ = (200 * math.sin(-playerAngleX) + (tes3mp.GetPosZ(pid) + 100))
+			object.location.posX = PosX
+			object.location.posY = PosY
+			object.location.posZ = PosZ
+			resendPlaceToAll(playerSelectedObject[pname], cell)			
+		end
+		if tes3mp.GetSneakState(pid) then		
+			tes3mp.MessageBox(pid, -1, trad.placeobjet)				
+			return false
+		else
+			tes3mp.RestartTimer(TimerDrop, time.seconds(0.01))	
+		end
 	end
 end
 
