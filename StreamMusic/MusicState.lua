e68110abbf4fea20b7d9b6ea5f54ba1609834d35
rickoff <38435435+rickoff@users.noreply.github.com>
2020-04-22 04:31:31 +0200

    Update MusicState.lua

--- StreamMusic/MusicState.lua
+++ StreamMusic/MusicState.lua
@@ -8,7 +8,6 @@ MusicState script
 INSTALLATION:
 Save the file as MusicState.lua inside your server/scripts/custom folder.
 Save files EcarlateNpc.json and EcarlateCreaIa.json in your custom data folder.
-
 Edits to customScripts.lua
 MusicState = require("custom.MusicState")
 ---------------------------
@@ -21,20 +20,19 @@ local MusicState = {}
 local NpcData = {}
 local NpcList = jsonInterface.load("custom/EcarlateNpc.json")
 for index, item in pairs(NpcList) do
-	if item.figth > 50 then
-		table.insert(NpcData, {NAME = item.name, REFID = string.lower(item.refid)})
+	if item.figth > 80 then
+		table.insert(NpcData, {REFID = string.lower(item.refid)})
 	end
 end
 
-local CreaData = {}
 local CreaList = jsonInterface.load("custom/EcarlateCreaIa.json")
 for index, item in pairs(CreaList) do
-	if item.figth > 50 then
-		table.insert(CreaData, {NAME = item.name, REFID = string.lower(item.refid)})
+	if item.figth > 60 then
+		table.insert(NpcData, {REFID = string.lower(item.refid)})
 	end
 end
 
-local Battle = {"Arena", "Assault", "Battle Intro", "MW battle1", "MW battle2", "MW battle3", "MW battle 4", "MW battle5", "MW battle 7", "MW battle 8"}
+local Battle = {"Arena", "Assault", "Battle Intro", "MW battle1", "Cimmerians Theme", "MW battle3", "Desperate Measures", "March of beast", "MW battle 7", "MW battle 8"}
 local Explore = {"mx_explore_1", "mx_explore_2", "mx_explore_3", "mx_explore_4", "mx_explore_5", "mx_explore_6", "mx_explore_7", "mx_explore_8", "mx_explore_9", "mx_explore_10"}
 
 local TimerMusicBattle = tes3mp.CreateTimer("FunctionMusicBattle", time.seconds(1))
@@ -44,10 +42,11 @@ local PlayerPid
 local MusicPlayerBattle = false
 
 local cfg = {}
-cfg.rad = 1500
+cfg.rad = 2000
 
 MusicState.StartCheck = function(eventStatus)
 	tes3mp.StartTimer(TimerMusicBattle)
+	tes3mp.StartTimer(TimerMusicExplore)	
 	tes3mp.LogAppend(enumerations.log.INFO, "....START TIMER MUSICSTATE....")		
 end
 
@@ -60,23 +59,6 @@ function FunctionMusicBattle()
 				local cellDescription = tes3mp.GetCell(pid)
 				local cell = LoadedCells[cellDescription]	
 				if cell ~= nil then
-					for _, uniqueIndex in pairs(cell.data.packets.spawn) do
-						if cell.data.objectData[uniqueIndex] then
-							if cell.data.objectData[uniqueIndex].refId and cell.data.objectData[uniqueIndex].location then
-								local creatureRefId = cell.data.objectData[uniqueIndex].refId
-								if tableHelper.containsValue(CreaData, string.lower(creatureRefId), true) then							
-									local creaturePosX = cell.data.objectData[uniqueIndex].location.posX
-									local creaturePosY = cell.data.objectData[uniqueIndex].location.posY
-									local distance = math.sqrt((playerPosX - creaturePosX) * (playerPosX - creaturePosX) + (playerPosY - creaturePosY) * (playerPosY - creaturePosY)) 
-									if distance < cfg.rad and not tableHelper.containsValue(cell.data.packets.death, uniqueIndex, true) then
-										PlayerPid = pid
-										MusicPlayerBattle = true
-										break
-									end
-								end
-							end
-						end
-					end
 					for _, uniqueIndex in pairs(cell.data.packets.actorList) do
 						if cell.data.objectData[uniqueIndex] then
 							if cell.data.objectData[uniqueIndex].refId and cell.data.objectData[uniqueIndex].location then
@@ -107,7 +89,6 @@ function FunctionMusicBattle()
 					end
 				end									
 				logicHandler.RunConsoleCommandOnPlayer(PlayerPid, 'Streammusic "battle/'..BattleMusic..'.mp3"', false)
-				tes3mp.StartTimer(TimerMusicExplore)
 			end
 		end		
 	end
@@ -116,64 +97,50 @@ end
 
 function FunctionMusicExplore()
 	local PlayerSafe = true
-	for pid ,value in pairs(Players) do
-		if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-			PlayerPid = pid
-			local playerPosX = tes3mp.GetPosX(pid)
-			local playerPosY = tes3mp.GetPosY(pid)
-			local cellDescription = tes3mp.GetCell(pid)
-			local cell = LoadedCells[cellDescription]	
-			if cell ~= nil then
-				for _, uniqueIndex in pairs(cell.data.packets.spawn) do
-					if cell.data.objectData[uniqueIndex] then
-						if cell.data.objectData[uniqueIndex].refId and cell.data.objectData[uniqueIndex].location then
-							local creatureRefId = cell.data.objectData[uniqueIndex].refId
-							if tableHelper.containsValue(CreaData, string.lower(creatureRefId), true) then							
-								local creaturePosX = cell.data.objectData[uniqueIndex].location.posX
-								local creaturePosY = cell.data.objectData[uniqueIndex].location.posY
-								local distance = math.sqrt((playerPosX - creaturePosX) * (playerPosX - creaturePosX) + (playerPosY - creaturePosY) * (playerPosY - creaturePosY)) 
-								if distance < cfg.rad and not tableHelper.containsValue(cell.data.packets.death, uniqueIndex, true) then
-									PlayerSafe = false
-									break
+	if MusicPlayerBattle == true then
+		for pid ,value in pairs(Players) do
+			if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
+				PlayerPid = pid
+				local playerPosX = tes3mp.GetPosX(pid)
+				local playerPosY = tes3mp.GetPosY(pid)
+				local cellDescription = tes3mp.GetCell(pid)
+				local cell = LoadedCells[cellDescription]	
+				if cell ~= nil then
+					for _, uniqueIndex in pairs(cell.data.packets.actorList) do
+						if cell.data.objectData[uniqueIndex] then
+							if cell.data.objectData[uniqueIndex].refId and cell.data.objectData[uniqueIndex].location then
+								local creatureRefId = cell.data.objectData[uniqueIndex].refId
+								if tableHelper.containsValue(NpcData, string.lower(creatureRefId), true) then							
+									local creaturePosX = cell.data.objectData[uniqueIndex].location.posX
+									local creaturePosY = cell.data.objectData[uniqueIndex].location.posY
+									local distance = math.sqrt((playerPosX - creaturePosX) * (playerPosX - creaturePosX) + (playerPosY - creaturePosY) * (playerPosY - creaturePosY)) 
+									if distance < cfg.rad and not tableHelper.containsValue(cell.data.packets.death, uniqueIndex, true) then
+										PlayerSafe = false
+										MusicPlayerBattle = true
+										break
+									end	
 								end
 							end
 						end
-					end
+					end					
 				end
-				for _, uniqueIndex in pairs(cell.data.packets.actorList) do
-					if cell.data.objectData[uniqueIndex] then
-						if cell.data.objectData[uniqueIndex].refId and cell.data.objectData[uniqueIndex].location then
-							local creatureRefId = cell.data.objectData[uniqueIndex].refId
-							if tableHelper.containsValue(NpcData, string.lower(creatureRefId), true) then							
-								local creaturePosX = cell.data.objectData[uniqueIndex].location.posX
-								local creaturePosY = cell.data.objectData[uniqueIndex].location.posY
-								local distance = math.sqrt((playerPosX - creaturePosX) * (playerPosX - creaturePosX) + (playerPosY - creaturePosY) * (playerPosY - creaturePosY)) 
-								if distance < cfg.rad and not tableHelper.containsValue(cell.data.packets.death, uniqueIndex, true) then
-									PlayerSafe = false
-									break
-								end	
-							end
-						end
+			end
+		end
+		if PlayerSafe == true and PlayerPid ~= nil then	
+			if Players[PlayerPid] ~= nil and Players[PlayerPid]:IsLoggedIn() then	
+				local rando = math.random(1, 10)
+				local ExploreMusic 		
+				for slot, music in pairs(Explore) do
+					if slot == rando then
+						ExploreMusic = music
 					end
-				end					
+				end		
+				logicHandler.RunConsoleCommandOnPlayer(PlayerPid, 'Streammusic "explore/'..ExploreMusic..'.mp3"', false)
+				MusicPlayerBattle = false
 			end
 		end
 	end
-	if PlayerSafe == true and PlayerPid ~= nil then	
-		if Players[PlayerPid] ~= nil and Players[PlayerPid]:IsLoggedIn() then	
-			local rando = math.random(1, 10)
-			local ExploreMusic 		
-			for slot, music in pairs(Explore) do
-				if slot == rando then
-					ExploreMusic = music
-				end
-			end		
-			logicHandler.RunConsoleCommandOnPlayer(PlayerPid, 'Streammusic "explore/'..ExploreMusic..'.mp3"', false)
-			MusicPlayerBattle = false
-		end
-	else
-		tes3mp.RestartTimer(TimerMusicExplore, time.seconds(1))	
-	end
+	tes3mp.RestartTimer(TimerMusicExplore, time.seconds(1))		
 end
 
 customEventHooks.registerHandler("OnServerInit", MusicState.StartCheck)
