4426b785893d39cefb3e24c06f96c6e2c89c965e
rickoff <38435435+rickoff@users.noreply.github.com>
2020-09-05 19:50:22 +0200

    Update VocalDiscord.lua

--- VoiceBot/VocalDiscord.lua
+++ VoiceBot/VocalDiscord.lua
@@ -6,12 +6,23 @@ DESCRIPTION :
 Check the player in discord vocal or not
 Use /vocal for active instancied vocal
 ---------------------------
+REQUIRE:
+create a bot on your discord
+drop folder luadiscord in server/lib
+edit the config.lua file in server/lib/luadiscord
+execute start.bat located in server/lib/luadiscord
+---------------------------
 INSTALLATION:
 Save the file as VocalDiscord.lua inside your server/scripts/custom folder.
+Save the file as userdiscord.lua inside your server/data/custom folder.
+Save the file as playerLocations.lua inside your server/data/custom folder.
 
 Edits to customScripts.lua
 VocalDiscord = require("custom.VocalDiscord")
 ---------------------------
+CONFIG:
+config.kickPlayer = true or config.kickPlayer = false if you want to kick players who are not in a vocal channel of your discord
+change time config.timerstartvocal = 60 to modify the delay before expulsion
 ]]
 
 jsonInterface = require("jsonInterface")
